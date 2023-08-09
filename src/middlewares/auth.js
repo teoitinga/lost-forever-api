57fb27413211a7f3d469708e6de542c48f63abd2
João Paulo Santana Gusmão <52930272+teoitinga@users.noreply.github.com>
2023-08-09 00:50:51 -0300

    implementando autenticação

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -1,9 +1,21 @@
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
+require('dotenv').config();
+
+
 module.exports = (req, res, next) => {
-    const { username, password} = req.body
-    if (username && password) {
-        return next()
+
+    const { user, password} = req.body
+
+    if (user == 'luiz' && password == '123') {
+            
+        const tokenObject = 1; //esse id vvem do banco de dados
+        const token = jwt.sign(JSON.stringify({ foo: 'bar' }), process.env.SECRET_KEY )
+        
+        res.status(200).json(token);
+
+    }else{
+
+        res.status(404).json({ message: 'Login inválido!' });
     }
-    return res.json({ 
-        message: 'usuário e/ou senha incorreta!'
-    })
 }
--- src/routes/login.js
+++ src/routes/login.js
@@ -1,29 +1,8 @@
-const jwt = require('jsonwebtoken');
-const bcrypt = require('bcryptjs');
-require('dotenv').config();
-
-/** @param { import('express').Express} app */
-
 const auth = require('../middlewares/auth')
 
 /** @param { import('express').Express} app */
-/*
-module.exports = app => {
-    app.get('/login', auth, (_, res) => {
-        res.send('Você foi logado com sucesso!')
-    })
-}
-*/
-module.exports = app => {
-    app.get('/login', (req, res) => {
-        if (req.body.user === 'luiz' && req.body.password === '123') {
-            
-            const tokenObject = 1; //esse id vvem do banco de dados
-            const token = jwt.sign(JSON.stringify({ foo: 'bar' }), process.env.SECRET_KEY )
-            res.status(200).json({ message: token });
-        }
-        res.status(500).json({ message: 'Login inválido!' });
 
-    })
 
+module.exports = app => {
+    app.get('/login', auth, (req, res) => {})
 }
